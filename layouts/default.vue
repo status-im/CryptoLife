e3cf67c083734a93b20a6dc958399d26b6d85bf5
BenSchZA <bscholtz.bds@gmail.com>
2018-10-27 01:24:49 +0200

    Clean up UI

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,7 @@
 <template>
-  <v-app dark>
+  <v-app light>
     <v-navigation-drawer
-      :mini-variant.sync="miniVariant"
-      :clipped="clipped"
+      clipped="false"
       v-model="drawer"
       fixed
       app
@@ -24,57 +23,18 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
+    <v-toolbar fixed app :clipped-left="true">
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer :fixed="true" app>
+
     </v-footer>
   </v-app>
 </template>
@@ -83,17 +43,12 @@
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
+        drawer: false,
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+          { icon: 'apps', title: 'Bonding', to: '/bonding' }
         ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        title: 'Protea v2.0'
       }
     }
   }
---
+++ pages/bonding.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-layout column justify-center align-center>
+        <div class="line-chart">
+            <line-chart :data="chartData" :options="chartOptions" />
+        </div>
+    </v-layout>
+</template>
+<script>
+  import LineChart from '../components/line-chart'
+  export default {
+    async asyncData ({ env }) {
+      return {
+        chartData: {
+          labels: [...Array.apply(null, {length: 100}).map(Number.call, Number)],
+          datasets: [
+            {
+              label: 'Community Bonding Curve',
+              backgroundColor: '#f87979',
+              data: [...[...Array.apply(null, {length: 100}).map(Number.call, Number)].map(y => Math.pow(2, 0.05 * y))]
+            }
+          ]
+        },
+        chartOptions: {
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'Price'
+              }
+            }],
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'Supply'
+              }
+            }]
+          }
+        }
+      }
+    },
+    components: {
+      LineChart
+    }
+  }
+</script>
