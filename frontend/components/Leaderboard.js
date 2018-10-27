282eb3b26429551374b92ab7eb58d3162b9cb1ae
geleeroyale <krassvs@gmail.com>
2018-10-27 18:23:38 +0200

    basic leaderboard styling

--- frontend/components/Leaderboard.js
+++ frontend/components/Leaderboard.js
@@ -1,35 +1,48 @@
 import React, { PureComponent } from 'react'
+import styled from 'styled-components'
+
+const TxWrapper = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+`
+const TxLink = styled.a`
+  color: #ff9a62;
+  text-decoration: none;
+`
 
 export default class Leaderboard extends PureComponent {
   state = {
-    txs: []
+    txs: [],
   }
 
-  fetchTxs = async (address) => {
+  fetchTxs = async address => {
     // FIXME: avoid using http://cors-anywhere.herokuapp.com
-    const url = `http://cors-anywhere.herokuapp.com/https://blockscout.com/eth/mainnet/api?module=account&action=txlist&address=${address}`;
-    const response = await fetch(url);
-    const json = await response.json();
-    this.setState({txs: json.result});
+    const url = `http://cors-anywhere.herokuapp.com/https://blockscout.com/eth/mainnet/api?module=account&action=txlist&address=${address}`
+    const response = await fetch(url)
+    const json = await response.json()
+    this.setState({ txs: json.result })
   }
 
   async componentDidMount() {
-    this.fetchTxs(this.props.address);
+    this.fetchTxs(this.props.address)
   }
 
   render() {
-    const TxsList = this.state.txs.map((tx) =>
-      <div key={tx.hash}>
-        <a href={`https://blockscout.com/eth/mainnet/tx/${tx.hash}`} target="_blank">
+    const TxsList = this.state.txs.map(tx => (
+      <TxWrapper key={tx.hash}>
+        <TxLink
+          href={`https://blockscout.com/eth/mainnet/tx/${tx.hash}`}
+          target="_blank"
+        >
           {tx.hash}
-        </a>
-      </div>
-    )
+        </TxLink>
+      </TxWrapper>
+    ))
     return (
-      <div>
-        <div>Txs:</div>
+      <TxWrapper>
+        <TxWrapper>Txs:</TxWrapper>
         {TxsList}
-      </div>
+      </TxWrapper>
     )
   }
 }
