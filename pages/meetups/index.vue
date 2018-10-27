922a44528a9145d56ebd0833d410b525c84f42aa
BenSchZA <bscholtz.bds@gmail.com>
2018-10-27 23:02:30 +0200

    Opt-in Web3 provider

--- pages/meetups/index.vue
+++ pages/meetups/index.vue
@@ -48,37 +48,36 @@
       this.$store.commit('setSnackbarMessage', 'Configuring Web3/Ethereum')
       this.$store.commit('openSnackbar')
       window.addEventListener('load', async () => {
-        // if (window.ethereum) {
-        //   window.web3 = new Web3(window.ethereum)
-        //   try {
-        //     // Request account access if needed
-        //     await window.ethereum.enable()
-        //     // Acccounts now exposed
-        //     this.$store.commit('setSnackbarMessage', 'Accounts exposed')
-        //     this.$store.commit('openSnackbar')
-        //     window.web3.eth.sendTransaction({/* ... */})
-        //   } catch (error) {
-        //     // User denied account access...
-        //     this.$store.commit('setSnackbarMessage', 'User denied account access')
-        //     this.$store.commit('openSnackbar')
-        //   }
-        // }
-        // // Legacy dapp browsers...
-        // else if (window.web3) {
-        //   window.web3 = new Web3(window.web3.currentProvider)
-        //   this.$store.commit('setSnackbarMessage', 'Legacy dapp browser')
-        //   this.$store.commit('openSnackbar')
-        //   // Acccounts always exposed
-        //   window.web3.eth.sendTransaction({/* ... */})
-        // }
-        // // Non-dapp browsers...
-        // else {
-        //   this.$store.commit('setSnackbarMessage', 'Non-Ethereum browser detected')
-        //   this.$store.commit('openSnackbar')
-        // }
-        window.web3 = new Web3(window.web3.currentProvider)
-        this.$store.commit('setSnackbarMessage', 'Legacy dapp browser')
-        this.$store.commit('openSnackbar')
+        if (window.ethereum) {
+          window.web3 = new Web3(window.ethereum)
+          try {
+            // Request account access if needed
+            await window.ethereum.enable()
+            // Acccounts now exposed
+            this.$store.commit('setSnackbarMessage', 'Accounts exposed')
+            this.$store.commit('openSnackbar')
+          } catch (error) {
+            // User denied account access...
+            this.$store.commit('setSnackbarMessage', 'User denied account access')
+            this.$store.commit('openSnackbar')
+          }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+          window.web3 = new Web3(window.web3.currentProvider)
+          this.$store.commit('setSnackbarMessage', 'Legacy dapp browser')
+          this.$store.commit('openSnackbar')
+          // Acccounts always exposed
+        }
+        // Non-dapp browsers...
+        else {
+          this.$store.commit('setSnackbarMessage', 'Non-Ethereum browser detected')
+          this.$store.commit('openSnackbar')
+        }
+
+        // window.web3 = new Web3(window.web3.currentProvider)
+        // this.$store.commit('setSnackbarMessage', 'Legacy dapp browser')
+        // this.$store.commit('openSnackbar')
         // Acccounts always exposed
         window.web3.eth.getAccounts().then(accounts => {
           window.web3.eth.defaultAccount = accounts[0]
