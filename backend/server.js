93046ad31460670f9f74380e3dbfcee4f6b075b0
danielivanov <daniel.k.ivanov95@gmail.com>
2018-10-27 13:07:28 +0200

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 'use strict';
 const express = require('express');
+const LimePayService = require('./services/limepay-service');
 
 const app = express();
 
 app.get('/payment', async (req, res, next) => {
-    res.json("success");
+    try {
+        let token = await LimePayService.createPayment("5bd445a3474600056cb35e07", "chorap", "1", "1");
+        res.json(token);
+    } catch (error) {
+        if (error.response && error.response.data) {
+            console.log(`Could not create payment. Details:\n${JSON.stringify(error.response.data)}`);
+        }
+        next(error);
+    }
+
 });
 
 app.use((err, request, response, next) => {
-    console.log(err);
     response.status(500).send("Something went wrong.");
 });
 
