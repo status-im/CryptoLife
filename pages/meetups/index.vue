9bac4b3a8bf0bee7d868494f67e2033b59ddb1e6
BenSchZA <bscholtz.bds@gmail.com>
2018-10-27 22:54:52 +0200

    Instantiate and call contract method

--- pages/meetups/index.vue
+++ pages/meetups/index.vue
@@ -48,34 +48,58 @@
       this.$store.commit('setSnackbarMessage', 'Configuring Web3/Ethereum')
       this.$store.commit('openSnackbar')
       window.addEventListener('load', async () => {
-        if (window.ethereum) {
-          window.web3 = new Web3(window.ethereum)
-          try {
-            // Request account access if needed
-            await window.ethereum.enable()
-            // Acccounts now exposed
-            this.$store.commit('setSnackbarMessage', 'Accounts exposed')
-            this.$store.commit('openSnackbar')
-            window.web3.eth.sendTransaction({/* ... */})
-          } catch (error) {
-            // User denied account access...
-            this.$store.commit('setSnackbarMessage', 'User denied account access')
-            this.$store.commit('openSnackbar')
-          }
-        }
-        // Legacy dapp browsers...
-        else if (window.web3) {
-          window.web3 = new Web3(window.web3.currentProvider)
-          this.$store.commit('setSnackbarMessage', 'Legacy dapp browser')
-          this.$store.commit('openSnackbar')
-          // Acccounts always exposed
-          window.web3.eth.sendTransaction({/* ... */})
-        }
-        // Non-dapp browsers...
-        else {
-          this.$store.commit('setSnackbarMessage', 'Non-Ethereum browser detected')
-          this.$store.commit('openSnackbar')
-        }
+        // if (window.ethereum) {
+        //   window.web3 = new Web3(window.ethereum)
+        //   try {
+        //     // Request account access if needed
+        //     await window.ethereum.enable()
+        //     // Acccounts now exposed
+        //     this.$store.commit('setSnackbarMessage', 'Accounts exposed')
+        //     this.$store.commit('openSnackbar')
+        //     window.web3.eth.sendTransaction({/* ... */})
+        //   } catch (error) {
+        //     // User denied account access...
+        //     this.$store.commit('setSnackbarMessage', 'User denied account access')
+        //     this.$store.commit('openSnackbar')
+        //   }
+        // }
+        // // Legacy dapp browsers...
+        // else if (window.web3) {
+        //   window.web3 = new Web3(window.web3.currentProvider)
+        //   this.$store.commit('setSnackbarMessage', 'Legacy dapp browser')
+        //   this.$store.commit('openSnackbar')
+        //   // Acccounts always exposed
+        //   window.web3.eth.sendTransaction({/* ... */})
+        // }
+        // // Non-dapp browsers...
+        // else {
+        //   this.$store.commit('setSnackbarMessage', 'Non-Ethereum browser detected')
+        //   this.$store.commit('openSnackbar')
+        // }
+        window.web3 = new Web3(window.web3.currentProvider)
+        this.$store.commit('setSnackbarMessage', 'Legacy dapp browser')
+        this.$store.commit('openSnackbar')
+        // Acccounts always exposed
+        window.web3.eth.getAccounts().then(accounts => {
+          window.web3.eth.defaultAccount = accounts[0]
+        }).finally(() => {
+          console.log('Creating contract instance')
+          let contract = new window.web3.eth.Contract(this.$store.state.communityTokenABI, this.$store.state.communityTokenAddress)
+
+          console.log('Sending method call')
+          console.log(window.web3.eth.defaultAccount)
+          contract.methods.burn(200).send({from: window.web3.eth.defaultAccount})
+            .on('transactionHash', function (hash) {
+              console.log(hash)
+            })
+            .on('receipt', function (receipt) {
+              console.log(receipt)
+            })
+            .on('confirmation', function (confirmationNumber, receipt) {
+              console.log(confirmationNumber)
+            })
+            .on('error', console.error)
+        })
       })
     },
     computed: {
--- plugins/web3-plugin.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import Web3 from 'web3'
-
-// explicit installation required in module environments
-Vue.use(Web3)
