f2fa882c3a5850957cee85ccedb52132eb709e92
vgorin <vgrn.at@gmail.com>
2018-10-27 15:37:33 +0200

    update truffle.js

--- truffle.js
+++ truffle.js
@@ -15,4 +15,29 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+
+	networks: {
+		development: {
+			provider: function () {
+				const wallet = new HDWalletProvider(
+					"***12 words***",
+					"https://rinkeby.infura.io/***key***"
+				);
+				const nonceTracker = new NonceTrackerSubprovider();
+				wallet.engine._providers.unshift(nonceTracker);
+				nonceTracker.setEngine(wallet.engine);
+				return wallet;
+			},
+			network_id: "*", // Match any network (determined by provider)
+			gas: 4500000,
+			gasPrice: 21000000000 // 21 GWei
+		},
+		test: {
+			host: "localhost",
+			network_id: "*",
+			port: 8666,
+			gas: 0xffffffff,
+			gasPrice: 1
+		}
+	},
 };
