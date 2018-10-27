d140aa6c35f78b985ab8f3f56d74fe846df68e72
Alejandro Avilés <omegak@gmail.com>
2018-10-27 03:58:30 +0200

    Introduce addreth as a first-class citizen :)

--- frontend/components/NotAnAddreth/index.js
+++ frontend/components/NotAnAddreth/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 
-export default class NotAnAddress extends PureComponent {
+export default class NotAnAddreth extends PureComponent {
   render() {
     return (
       <div>Wait a sec... This doesn't smell like an Ethereum address nor ENS domain!</div>
---
+++ frontend/pages/addreth.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import parse from 'domain-name-parser'
+
+import NotAnAddreth from '../components/NotAnAddreth'
+
+export default class Addreth extends Component {
+  static async getInitialProps({ query }) {
+    return query;
+  }
+
+  validateENSDomain(addreth) {
+    const domain = parse(addreth);
+    return domain.tld == 'eth';
+  }
+
+  validateAddreth(addreth) {
+    const web3 = new Web3();
+    return web3.utils.isAddress(addreth) || this.validateENSDomain(addreth);
+  }
+
+  render() {
+    const isAddrethValid = this.validateAddreth(this.props.addreth);
+    return (
+      <div>
+        {this.props.addreth}
+        {!isAddrethValid &&
+          <NotAnAddreth/>
+        }
+      </div>
+    )
+  }
+}
