03ca1366ea82021b59ae0cae72814d4e7eefaa0f
danielivanov <daniel.k.ivanov95@gmail.com>
2018-10-27 10:30:48 +0200

    remove escrow and deploy

--- dapp/deployment/deploy.js
+++ dapp/deployment/deploy.js
@@ -6,7 +6,6 @@ const deploy = async (network, secret) => {
 
 	const deployer = new etherlime.InfuraPrivateKeyDeployer(secret, network);
 	const tokenContractDeployed = await deployer.deploy(Token, {});
-	const escrowContractDeployed = await deployer.deploy(EscrowContract, {}, tokenContractDeployed.contract.address);
 };
 
 module.exports = {
