f5d6fbb4d4fc656297c714f5f54d0d4e6effc6d2
Johann Barbie <johannbarbie@me.com>
2018-10-27 11:54:20 +0200

    life is not what it seems

--- chainOfLife/contracts/Life.sol
+++ chainOfLife/contracts/Life.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.4.24;
 contract Life {
     
     function life(bytes32[] _dish) public pure returns (bytes32[] _newGen) {
-        _newGen = new bytes32[](_dish.length);
+        //_newGen = new bytes32[](_dish.length);
+        _newGen = new bytes32[](8);
+        bool isAlive;
+        uint256 neighbors;
         //for (uint8 row = 0; row < _dish.length; row++) { //each row
-        for (uint8 row = 0; row < 8; row ++) { //each row
+        for (uint8 row = 1; row < 4; row++) { //each row
           //for (uint8 cell = 0; row < _dish.length; cell++) { //each cell
-          for (uint8 cell = 1; cell < 8; cell ++) { //each cell
-            bool isAlive = checkAlive(_dish[row], cell);
-            uint256 neighbors = 3; //getNeighbours(_dish[row - 1], _dish[row], _dish[row +1], cell);
+          for (uint8 cell = 1; cell < 8; cell++) { //each cell
+            isAlive = checkAlive(_dish[row], cell);
+            neighbors = getNeighbours(_dish[row - 1], _dish[row], _dish[row + 1], cell);
             if (!isAlive && neighbors == 3) {
                 _newGen[row] = _newGen[row] | bytes32(0x01 << cell);
             } else {
@@ -30,15 +33,15 @@ contract Life {
     }
     
     function countAncestors(bytes32 _field, uint256 _start, uint256 _count) internal pure returns (uint256 count) {
-      for (uint i = _start; i < _start + _count; i++) {
+      for (uint256 i = _start; i < _start + _count; i++) {
         count += uint8(_field >> i) & 0x01;
       }
     }
     
     function getNeighbours(bytes32 _above, bytes32 _same,bytes32 _below, uint8 _pos) public pure returns (uint256 ancestors) {
-		ancestors += countAncestors(_above, _pos - 1, 3);
-		ancestors += countAncestors(_same, _pos - 1, 1);
-		ancestors += countAncestors(_same, _pos + 1, 1);
-		ancestors += countAncestors(_below, _pos - 1, 3);
+        ancestors += countAncestors(_above, _pos - 1, 3);
+        ancestors += countAncestors(_same, _pos - 1, 1);
+        ancestors += countAncestors(_same, _pos + 1, 1);
+        ancestors += countAncestors(_below, _pos - 1, 3);
     }
 }
