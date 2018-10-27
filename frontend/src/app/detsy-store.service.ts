77e1b7770157db4734ca5393f8ee877017f84434
Vladdy <ivanov.vladdy@gmail.com>
2018-10-27 14:16:49 +0200

    Update first row of items

--- frontend/src/app/detsy-store.service.ts
+++ frontend/src/app/detsy-store.service.ts
@@ -9,18 +9,20 @@ export class DetsyStoreService {
 		DETSY_STORE.set('8598d4bc54f247a4bdeee3a6a2d177f3', {
 			id: '8598d4bc54f247a4bdeee3a6a2d177f3',
 			qty: 1,
-			name: 'Blue Leather Bracelet White Flag of Argentina',
-			description: 'With a velvet pouch\n' +
-			'Material: alloy, leather\n' +
-			'Width: 1.4 cm\n' +
-			'Length: 20 / 21.5 / 23 cm\n' +
-			'Color: blue white\n' +
-			'Package Contents:\n' +
-			'1 x belt\n' +
-			'1 x velvet bag\n' +
-			'Note: Light shooting and different displays may cause the color of the item in the picture a little different from the real thing. The measurement allowed error is +/- 1-3cm.',
+			name: 'For Lovers Best Friendship Flag Of Venezuela',
+			description: 'Item Type: Necklaces\n' +
+			'Necklace Type: Pendant Necklaces\n' +
+			'Gender: Womens , Mens , Kids\n' +
+			'Metal: Alloy \n' +
+			'Material: Glass\n' +
+			'Color: Black + Alloy\n' +
+			'Chain Type: Black Leather Link Chain\n' +
+			'Style:Trendy\n' +
+			'Chain Length: 50+5cm\n' +
+			'Cabochon Diameter: 20*27mm \n' +
+			'Package Including: 1 piece necklace',
 			price: '1 DAI',
-			imagePath: '/assets/images/leather-bracelet-argentina.jpg'
+			imagePath: '/assets/images/flag-of-venezuela.jpg'
 		});
 
 		DETSY_STORE.set('9d2a900a7e2a4ce6a8ac114b2cde8862', {
@@ -43,18 +45,10 @@ export class DetsyStoreService {
 		DETSY_STORE.set('dbb51d51747a442cb4c0b52a74ae3805', {
 			id: 'dbb51d51747a442cb4c0b52a74ae3805',
 			qty: 1,
-			name: 'Blue Leather Bracelet White Flag of Argentina',
-			description: 'With a velvet pouch\n' +
-			'Material: alloy, leather\n' +
-			'Width: 1.4 cm\n' +
-			'Length: 20 / 21.5 / 23 cm\n' +
-			'Color: blue white\n' +
-			'Package Contents:\n' +
-			'1 x belt\n' +
-			'1 x velvet bag\n' +
-			'Note: Light shooting and different displays may cause the color of the item in the picture a little different from the real thing. The measurement allowed error is +/- 1-3cm.',
+			name: 'Soft Unicorn Doll ',
+			description: 'Kawaii Plush Toy Soft Unicorn Doll Appease Sleeping Pillow Kids Room Decor Toy For Children Pupil Christmas Halloween present',
 			price: '1 DAI',
-			imagePath: '/assets/images/leather-bracelet-argentina.jpg'
+			imagePath: '/assets/images/unicorn.jpg'
 		});
 
 		DETSY_STORE.set('453e9847086a4993bb526a350b6ae252', {
