dc94884382cfb364e28ecac2c73eaff1eb4357cf
eshavkun <dmoroz.thrash@gmail.com>
2018-10-27 15:41:28 +0200

    'resolve' function and test

--- chainOfLife/contracts/ChainOfLife.sol
+++ chainOfLife/contracts/ChainOfLife.sol
@@ -2,8 +2,11 @@ pragma solidity ^0.4.24;
 
 contract ChainOfLife {
 
-  event StartGame(bytes32 indexed gameId, address indexed alice);
-  event JoinGame(bytes32 indexed gameId, address indexed bob, bytes32[] field);
+  event StartGame(bytes32 indexed gameId, address indexed playerOne);
+  event JoinGame(bytes32 indexed gameId, address indexed playerTwo, bytes32[] PlayerTwoField);
+  event ResolveGame(bytes32 indexed gameId, address indexed winner, bytes32[] PLayerOneField);
+
+  event LogHash(string message, bytes32 value);
 
   struct Game{
     address alice;
@@ -21,7 +24,13 @@ contract ChainOfLife {
     uint256 time;
   }
 
+  struct PlayerStats{
+    uint games;
+    uint wins;
+  }
+
   mapping (bytes32 => Game) public games;
+  mapping (address => PlayerStats) public players;
 
   function register(bytes32 _hash) public {
     games[_hash] = Game(msg.sender, 0, 0, 0, block.timestamp);
@@ -39,5 +48,26 @@ contract ChainOfLife {
     game.time = block.timestamp;
 
     emit JoinGame(_gameId, msg.sender, _field);
+  }
+
+  function resolve(bytes32 _gameId, bytes32[] _field, bool _isWinner) public {
+    bytes32 fieldHash = keccak256(abi.encodePacked(_field));
+    require(_gameId == fieldHash, "Alice hashes do not match");
+
+    Game storage game = games[_gameId];
+    require(msg.sender == game.alice, "Resolve message is not sent by Alice");
+
+    address winner;
+    if (_isWinner){
+      winner = game.alice;
+      game.state = 2;
+      game.time = block.timestamp;
+    } else {
+      winner = game.bob;
+      game.state = 3;
+      game.time = block.timestamp;
+    }
+
+    emit ResolveGame(_gameId, winner, _field);
   }
 }
