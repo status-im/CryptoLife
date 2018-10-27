50194e9f4f802cae5369a386dff205ccccfbb330
deam <deamme@users.noreply.github.com>
2018-10-27 05:25:16 +0200

    Cleanup

--- frontend/pages/claim.js
+++ frontend/pages/claim.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import styled, { injectGlobal } from 'styled-components'
 import styledNormalize from 'styled-normalize'
 import Head from 'next/head'
-import MetamaskSigner from '../../components/MetamaskSigner/MetamaskSigner.js'
+import MetamaskSigner from '../../components/MetamaskSigner'
 
 export default class Claim extends Component {
   render() {
     return (
-      <div><MetamaskSigner /></div>
+      <div>
+        <MetamaskSigner />
+      </div>
     )
   }
 }
-
