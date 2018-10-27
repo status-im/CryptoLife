ad984472a3aec4963b8c23e924cd8cb017e6b1af
Johann Barbie <johannbarbie@me.com>
2018-10-27 17:59:35 +0200

    life takes from sea to land to air

--- chainOfLife/contracts/Life.sol
+++ chainOfLife/contracts/Life.sol
@@ -4,6 +4,7 @@ contract Life {
 
   // 0x01 == 00000001 = white
   // 0x02 == 00000010 = black
+
     
   function life(bytes32[] _dish) public pure returns (bytes32[] _newGen) {
     _newGen = new bytes32[](_dish.length);
@@ -19,29 +20,29 @@ contract Life {
         color = (uint8(_dish[row] >> cell) & 0x03);
         prevRow = (row > 0) ? _dish[row - 1] : _dish[_dish.length-1];
         nextRow = (row < _dish.length - 1) ? _dish[row + 1] : _dish[0];
-        prevCell = (cell > 0) ? cell - 2 : 255;
-        nextCell = (cell < _dish.length - 2) ? cell + 2 : 0;
+        prevCell = (cell > 0) ? cell - 2 : _dish.length * 2 - 2;
+        nextCell = (cell < (_dish.length  * 2) - 2) ? cell + 2 : 0;
         aN = 0;
         bN = 0;
         // above
         aN += uint8(prevRow >> prevCell) & 0x01;
         aN += uint8(prevRow >> cell) & 0x01;
         aN += uint8(prevRow >> nextCell) & 0x01;
-        bN += (uint8(prevRow >> prevCell) & 0x02) == 2 ? 1 : 0;
-        bN += (uint8(prevRow >> cell) & 0x02) == 2 ? 1 : 0;
-        bN += (uint8(prevRow >> nextCell) & 0x02) == 2 ? 1 : 0;
+        bN += (uint8(prevRow >> prevCell) & 0x02) >> 1;
+        bN += (uint8(prevRow >> cell) & 0x02) >> 1;
+        bN += (uint8(prevRow >> nextCell) & 0x02) >> 1;
         // same
         aN += uint8(_dish[row] >> prevCell) & 0x01;
         aN += uint8(_dish[row] >> nextCell) & 0x01;
-        bN += (uint8(_dish[row] >> prevCell) & 0x02) == 2 ? 1 : 0;
-        bN += (uint8(_dish[row] >> nextCell) & 0x02) == 2 ? 1 : 0;
+        bN += (uint8(_dish[row] >> prevCell) & 0x02) >> 1;
+        bN += (uint8(_dish[row] >> nextCell) & 0x02) >> 1;
         // below
         aN += uint8(nextRow >> prevCell) & 0x01;
         aN += uint8(nextRow >> cell) & 0x01;
         aN += uint8(nextRow >> nextCell) & 0x01;
-        bN += (uint8(nextRow >> prevCell) & 0x02) == 2 ? 1 : 0;
-        bN += (uint8(nextRow >> cell) & 0x02) == 2 ? 1 : 0;
-        bN += (uint8(nextRow >> nextCell) & 0x02) == 2 ? 1 : 0;
+        bN += (uint8(nextRow >> prevCell) & 0x02) >> 1;
+        bN += (uint8(nextRow >> cell) & 0x02) >> 1;
+        bN += (uint8(nextRow >> nextCell) & 0x02) >> 1;
         if (color == 0) { // empty
           if (aN == 3) {
             if (bN == 3) {
