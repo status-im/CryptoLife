0496a1d368b62e3ff41dc6882ea14cdc92f2de1d
Alejandro Avilés <omegak@gmail.com>
2018-10-27 05:18:40 +0200

    Add link to address in BlockScout

--- frontend/pages/addreth.js
+++ frontend/pages/addreth.js
@@ -24,7 +24,7 @@ export default class Addreth extends Component {
     const isAddrethValid = this.validateAddreth(this.props.addreth);
     return (
       <div>
-        {this.props.addreth}
+        <a href={`https://blockscout.com/eth/mainnet/address/${this.props.addreth}`}>{this.props.addreth}</a>
         <Leaderboard address={this.props.addreth}/>
         {!isAddrethValid &&
           <NotAnAddreth/>
