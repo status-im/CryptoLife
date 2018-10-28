4dc9c2d4c9147170115b0c066271530cfe4a499a
Nelson Castillo <nelsoneci@gmail.com>
2018-10-28 09:59:33 +0100

    Move contracts to right directory and modify contract address.

--- CryptoTk/disc_jockey/run.sh
+++ CryptoTk/disc_jockey/run.sh
@@ -2,7 +2,7 @@
 
 export PUB_PATH=$HOME/git/pub.txt
 export PRIV_PATH=$HOME/git/priv.txt # Dat security :-P
-export NFT_CONTRACT=0xaa3f7043a1d1f59fb2e0f9c62ed682e0450308b1
+export NFT_CONTRACT=0x4abdfa122d59333112dee0ace0a7e8c9cea18d6b
 export PY3_TO_USE=$HOME/git/dj/bin/python3
 export WEB3_PROVIDER_URI=https://dai.poa.network/
 $PY3_TO_USE $@
