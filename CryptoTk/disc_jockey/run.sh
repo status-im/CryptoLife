d81a2bbae237db55087f9507d98e2ad4e63d97fc
Nelson Castillo <nelsoneci@gmail.com>
2018-10-28 06:11:17 +0100

    Run stuff - Do not leak stuff.

---
+++ CryptoTk/disc_jockey/run.sh
@@ -0,0 +1,8 @@
+# Run dj.
+
+export PUB_PATH=$HOME/git/pub.txt
+export PRIV_PATH=$HOME/git/priv.txt # Dat security :-P
+export NFT_CONTRACT=0xc885d35407c99468dd6aa99427d8834febc7f628
+export PY3_TO_USE=$HOME/git/dj/bin/python3
+export WEB3_PROVIDER_URI=https://dai.poa.network/
+$PY3_TO_USE calls.py
