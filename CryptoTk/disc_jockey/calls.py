a1d6e4a637008646785c0ea2b0d81b9b804516e8
Nelson Castillo <nelsoneci@gmail.com>
2018-10-28 07:59:20 +0100

    We can call stuff. Yay.

--- CryptoTk/disc_jockey/calls.py
+++ CryptoTk/disc_jockey/calls.py
@@ -10,12 +10,12 @@ def get_nft():
     return network.contract(address=network.to_checksum(addr.NFT_CONTRACT), abi=open('cryptotk.abi').read().strip())
 
 def wait_for_tx(tx_hash):
-    ' Wait for tx - ignore return value '
-    network.WEB3.eth.waitForTransactionReceipt(tx_hash)
+    ' Wait for tx '
+    return network.WEB3.eth.waitForTransactionReceipt(tx_hash)
 
-def vote(method, media_id, nonce_inc=0):
-    ' upvote. method=voteUp or voteDown. '
-    data_to_encode = get_nft().encodeABI(fn_name=method, args=[media_id])
+def single_arg(method, arg, nonce_inc):
+    ' make calls to the contract with a single arg - what we need for now - hackaton style '
+    data_to_encode = get_nft().encodeABI(fn_name=method, args=[arg])
     signed_txn = network.WEB3.eth.account.signTransaction(dict(
         nonce = network.WEB3.eth.getTransactionCount(network.to_checksum(addr.PUB)) + nonce_inc,
         gasPrice = GAS_PRICE,
@@ -25,6 +25,14 @@ def vote(method, media_id, nonce_inc=0):
         ), addr.PRIV)
     tx_hash = network.WEB3.eth.sendRawTransaction(signed_txn.rawTransaction)
     return tx_hash
+
+def vote(method, media_id, nonce_inc=0):
+    ' upvote. method=voteUp or voteDown. '
+    return single_arg(method, media_id, nonce_inc)
+
+def add_media(method, nft_uri, nonce_inc=0):
+    ' register new file '
+    return single_arg(method, nft_uri, nonce_inc)
 
 def send_xdai(dest, amount_wei, nonce_inc=0):
     ' Send xdai away '
@@ -39,11 +47,11 @@ def send_xdai(dest, amount_wei, nonce_inc=0):
 
 def test_vote():
   ' send a vote '
-  wait_for_tx(vote('voteUp', 0))
+  print(wait_for_tx(vote('voteUp', 0)))
 
 def main():
   ' main '
-  # test_vote() # OK
+  test_vote() # OK
 
 if __name__ == "__main__":
     main()
