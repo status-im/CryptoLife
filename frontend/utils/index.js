c1088e7041a962bf8fb85c0d524faa93c149a61b
Alejandro Avilés <omegak@gmail.com>
2018-10-28 03:31:53 +0100

    Add helper to get browser eth address

--- frontend/utils/index.js
+++ frontend/utils/index.js
@@ -4,12 +4,35 @@ import HttpProvider from 'ethjs-provider-http'
 // TODO: Allow configuring mainnet/ropsten
 
 export default class {
+  static async setWeb3() {
+    if (window.ethereum) {
+      await ethereum.enable()
+    }
+    if (window.ethereum || window.web3) {
+      window.web3 = new Web3(ethereum)
+    } else {
+      console.error("Non-Ethereum browser detected. You should consider trying MetaMask!")
+    }
+    console.log("Web3 set up!");
+  }
+
   static getEthereumHttpProvider() {
+    this.setWeb3()
     if (typeof window === 'object' && typeof window.web3 !== 'undefined') {
-      return web3.currentProvider;
+      return web3.currentProvider
     } else {
       const provider = new HttpProvider('https://mainnet.infura.io');
-      return provider;
+      return provider
+    }
+  }
+
+  static async getMyAddress() {
+    this.setWeb3()
+    try {
+      const accounts = await web3.eth.getAccounts()
+      return accounts[0].toLowerCase()
+    } catch (e) {
+      console.error(e)
     }
   }
 
